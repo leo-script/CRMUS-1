39bec6d33aa624700883b992f91c7d959bd49b07
samayuvi <74966092+samayuvi@users.noreply.github.com>
2021-03-09 11:36:58 +0600

    Update DBController.java

--- Mrex/src/com/company/LowLevelOperation/DBController.java
+++ Mrex/src/com/company/LowLevelOperation/DBController.java
@@ -7,9 +7,10 @@ import java.sql.SQLException;
 public class DBController {
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         String connectionUrl = "jdbc:postgresql://database-1.c1ipwagrda55.us-east-2.rds.amazonaws.com:5432/java_crm";
+        String passwordDb;
         try {
             Class.forName("org.postgresql.Driver");
-            Connection con = DriverManager.getConnection(connectionUrl, "postgres", "QhEO56Qvn2aElhLhh0SO");
+            Connection con = DriverManager.getConnection(connectionUrl, "postgres", passwordDb);
             return con;
         } catch (Exception e) {
             System.out.println("Exception occurred: " + e);
